ec017a7a8f4f4178ab8a7955165e22a16c74889f
chatgeant1 <vantuan2120@gmail.com>
2026-06-22 01:51:04 +0700

    2z

--- Client.java
+++ Client.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
-import javax.net.ssl.SSLSocket;
+
 
 public class Client implements Runnable{
     private Socket client;
